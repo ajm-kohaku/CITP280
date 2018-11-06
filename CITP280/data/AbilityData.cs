59e3b8b6dc96e7ddbe478080381ef5b88005ab25
Niertis Evani <niertis@gmail.com>
2018-11-06 18:47:11 -0500

    recovering part 3

---
+++ CITP280/data/AbilityData.cs
@@ -0,0 +1,19 @@
+﻿using CITP280.util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CITP280.data
+{
+    public class AbilityData
+    {
+        public CoreAttributes AbilityName { get; set; }
+        public int BaseScore { get; set; }
+        public int EnhancementScore { get; set; }
+        public int InherentScore { get; set; }
+        public int TempScore { get; set; }
+        public int PenaltyScore { get; set; }
+    }
+}
---
+++ CITP280/data/CharacterSheetData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CITP280.data
+{
+    public class CharacterSheetData
+    {
+        public AbilityData Strength { get; set; }
+        public AbilityData Dexterity { get; set; }
+        public AbilityData Constitution { get; set; }
+        public AbilityData Intelligence { get; set; }
+        public AbilityData Wisdom { get; set; }
+        public AbilityData Charisma { get; set; }
+        public int DamageTaken { get; set; }
+        public int MaxHealth { get; set; }
+        public int PlayerClassLevel { get; set; }
+        public string PlayerClassName { get; set; }
+        public int FavoredClassSkillCount { get; set; }
+        public int FavoredClassHealthCount { get; set; }
+        public int FavoredClassOtherCount { get; set; }
+
+    }
+}
