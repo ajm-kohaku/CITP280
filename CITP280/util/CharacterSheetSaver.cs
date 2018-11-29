effb5fae91e6de41be0464f22718c9343a4dd489
Niertis Evani <niertis@gmail.com>
2018-11-29 01:30:32 -0500

    part4 commit

---
+++ CITP280/util/CharacterSheetSaver.cs
@@ -0,0 +1,43 @@
+﻿using CITP280.data;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CITP280.util
+{
+    class CharacterSheetSaver
+    {
+        private Object saveLock = new Object();
+        private string characterSheetFullPath;
+        private CharacterSheetData characterSheet;
+
+        public CharacterSheetSaver(string characterSheetFullPath, CharacterSheetData characterSheet)
+        {
+            this.characterSheetFullPath = characterSheetFullPath;
+            this.characterSheet = characterSheet;
+        }
+
+        public void SaveCharacterSheet()
+        {
+            lock(saveLock)
+            {
+                // serialize JSON to a string and then write string to a file
+                File.WriteAllText(characterSheetFullPath, JsonConvert.SerializeObject(characterSheet, Formatting.Indented));
+            }
+        }
+
+        public string LoadCharacterSheet(String filePath)
+        {
+            lock(saveLock)
+            {
+              return  File.ReadAllText(filePath);
+            }
+        }
+    
+    }
+}
---
+++ CITP280/util/PlayableClassList.cs
@@ -0,0 +1,34 @@
+﻿using CITP280.playerclass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CITP280.util
+{
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"> accepts generic class of type BaseClass</typeparam>
+    public class PlayableClassList<T> where T: BaseClass
+    {
+        public string ClassName(T playableClass)
+        {
+            return playableClass.ClassName;
+        }
+
+        /// <summary>
+        /// Adds player class to the combobox
+        /// </summary>
+        /// <param name="comboBox">dropdown list the displays the class list</param>
+        /// <param name="playerClass">values that feed the list</param>
+        public void Add(ComboBox comboBox, T playerClass)
+        {
+            comboBox.Items.Add(playerClass.ClassName);
+        }
+       
+    }
+}
