297069caf0dc63ff2bde3214b5f598f4b5bd0631
Niertis Evani <niertis@gmail.com>
2018-12-09 14:33:02 -0500

    attempting to streamline class recorder section

---
+++ CITP280/util/ClassStatChangedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CITP280.util
+{
+    class ClassStatChangedEventArgs : EventArgs
+    {
+        public Int32 ConstitutionMod { get; set; }
+        public Int32 IntelligenceMod { get; set; }
+    }
+}
