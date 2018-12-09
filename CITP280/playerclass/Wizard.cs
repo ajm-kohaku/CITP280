13ee9fb7bb2225105a5178be7dfbde358826b502
niertis <niertis@gmail.com>
2018-12-09 15:51:48 -0500

    adding the rest of the "core" classes

---
+++ CITP280/playerclass/Wizard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CITP280.util;
+
+namespace CITP280.playerclass
+{
+    public class Sorcerer : BaseClass
+    {
+        public override string ClassName => "Sorcerer";
+
+        public override BaseAttackScale BaseAttack => BaseAttackScale.HALF;
+
+        public override SavesScale FortitudeScale => SavesScale.POOR;
+
+        public override SavesScale ReflexScale => SavesScale.POOR;
+
+        public override SavesScale WillScale => SavesScale.GOOD;
+
+        public override int SkillRanksPerLevel => 2;
+
+        public override int DieType => 6;
+        
+    }
+}
