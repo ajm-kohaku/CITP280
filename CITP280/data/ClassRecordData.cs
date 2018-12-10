8d1cb9de88e965c43f75d6b540d38d3ba15b9106
niertis <niertis@gmail.com>
2018-12-09 23:16:55 -0500

    added save file capabilities rearranged controls for presentation

---
+++ CITP280/data/ClassRecordData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CITP280.data
+{
+    public class ClassRecordData
+    {
+        public string MaxHealth { get; set; }
+        public string PlayerClassLevel { get; set; }
+        public string PlayerClassName { get; set; }
+        public string FavoredClassSkillCount { get; set; }
+        public string FavoredClassHealthCount { get; set; }
+        public string FavoredClassOtherCount { get; set; }
+    }
+}
---
+++ CITP280/playerclass/CoreClass.cs
@@ -0,0 +1,24 @@
+﻿using CITP280.util;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CITP280.playerclass
+{
+    //base class for most playable classes. child tables will have distinct features in phase 2.
+    public abstract class BaseClass : IPlayableClass
+    {
+        public ClassTypes ClassType => ClassTypes.BASE;
+        public abstract string ClassName { get; }
+        public abstract BaseAttackScale BaseAttack { get; }
+        public abstract SavesScale FortitudeScale { get; }
+        public abstract SavesScale ReflexScale { get; }
+        public abstract SavesScale WillScale { get; }
+        public abstract int SkillRanksPerLevel { get; }
+        public abstract int DieType { get; }
+        public int MaxLevel => 20;
+    }
+}
