e52a288a85836f2e3136f0e80ed363a98478bfa7
Niertis Evani <niertis@gmail.com>
2018-12-11 18:57:13 -0500

    adding changes for db

---
+++ CITP280/data/AttributeData.cs
@@ -0,0 +1,19 @@
+﻿using CITP280.util;
+using System;
+
+namespace CITP280.data
+{
+    public struct AttributeData
+    {
+        //pk for the table
+        public int AttributeId { get; }
+        //fk for the table. 
+        public int CharacterId { get; set; }
+        public CoreAttributes AbilityName { get; set; }
+        public int BaseScore { get; set; }
+        public int EnhancementScore { get; set; }
+        public int InherentScore { get; set; }
+        public int TempScore { get; set; }
+        public int PenaltyScore { get; set; }
+    }
+}
---
+++ CITP280/data/CharacterClassData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CITP280.data
+{
+    public struct CharacterClassData
+    {
+        public int CharacterClassId { get; }
+        public int CharacterId { get; set; }
+        public int MaxHealth { get; set; }
+        public int PlayerClassLevel { get; set; }
+        public string PlayerClassName { get; set; }
+        public int FavoredClassSkillCount { get; set; }
+        public int FavoredClassHealthCount { get; set; }
+        public int FavoredClassOtherCount { get; set; }
+    }
+}
